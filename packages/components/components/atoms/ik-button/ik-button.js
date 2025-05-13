ffe64ad52f23cff795e6d477cb18f1cd56a141cb
Siori9 <clara.layus@hotmail.fr>
2025-05-13 14:41:19 +0200

    feat(components): add ik-button component

--- packages/components/components/atoms/ik-button/ik-button.js
+++ packages/components/components/atoms/ik-button/ik-button.js
@@ -1,28 +1,142 @@
 import {css, html, LitElement} from "lit";
+import 'iconify-icon';
 
 class IkButton extends LitElement {
     static properties = {
         text: { type: String },
+        width: { type: String },
+        height: { type: String },
+        fontSize: { type: String },
+        img: { type: String },
+        icon: { type: String },
+        iconSize: { type: String },
+        type: { type: String },
     };
 
     constructor() {
         super();
-        this.text = 'Click me!';
+        this.text = undefined;
+        this.width = '220px';
+        this.height = '84px';
+        this.fontSize = '25px';
+        this.img = undefined;
+        this.icon = undefined;
+        this.iconSize = '40px';
+        this.type = 'full';
+    }
+
+    renderImg(){
+        console.log(this.img)
+        if(this.img !== undefined){
+            return html`<img class="icon" src="${this.img}" alt="button-image">`
+        }
+    }
+
+    renderIcon() {
+        if(this.icon !== undefined){
+            return html `
+                <iconify-icon
+                        class="icon"
+                        icon="${this.icon}"
+                </iconify-icon>
+            `
+        }
+    }
+
+    renderText() {
+        if(this.text !== undefined){
+            return this.text
+        }
     }
 
     render() {
-        return html`<button class="ik-button">${this.text}</button>`;
+        return html`
+            <button class="ik-button ${this.type}" 
+                    style="
+                        --btn-width: ${this.width}; 
+                        --btn-height: ${this.height};
+                        --btn-font-size: ${this.fontSize};
+                        --btn-icon-size: ${this.iconSize};
+                    "
+            >
+                ${this.renderIcon()}
+                ${this.renderImg()}
+                ${this.renderText()}
+            </button>`;
     }
+
 
     static styles = [
         css`
             .ik-button {
+                width: var(--btn-width);
+                height: var(--btn-height);
+                border-radius: 8px;
+                font-size: var(--btn-font-size);
+                padding: 0.5em 1em;
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: center;
+                gap: 0.3em;
+                cursor: pointer;
+            }
+
+            .ik-button.full {
                 background-color: var(--btn-bg, #242424);
                 color: var(--btn-text, #fff);
-                width: 220px;
-                height: 84px;
-                border-radius: 8px;
-                font-size: 25px;
+                border: none;
+            }
+
+            .ik-button.outlined {
+                background-color: var(--bg, #242424);
+                color: var(--text, #000);
+                border: solid 2px var(--text, #000);
+            }
+            
+            .ik-button.blue {
+                background-color: var(--btn-blue-bg, #0B006B);
+                color: var(--btn-text, #fff);
+                border: none;
+            }
+            
+            .ik-button.red {
+                background-color: var(--btn-red-bg, #8E1919);
+                color: var(--btn-text, #fff);
+                border: none;
+            }
+            
+            .ik-button.icon {
+                background-color: transparent;
+                color: var(--btn-text, #fff);
+                border: none;
+            }
+
+            .ik-button:hover.full {
+                background-color: var(--btn-hover-bg, #434343);
+            }
+
+            .ik-button:hover.outlined {
+                background-color: var(--btn-outlined-hover-bg, #a8a8a8);
+            }
+            
+            .ik-button:hover.blue {
+                background-color: var(--btn-blue-hover-bg, #b6b6b6);
+            }
+            
+            .ik-button:hover.red {
+                background-color: var(--btn-red-hover-bg, #af1f1f);
+            }
+            
+            .icon {
+                font-size: var(--btn-icon-size);
+                width: var(--btn-icon-size);
+                height: auto;
+                color: var(--text, #fff);;
+            }
+            
+            .icon:hover {
+                color: var(--text-hover, #232323);
             }
         `
     ];
--- packages/components/components/atoms/ik-button/ik-button.stories.js
+++ packages/components/components/atoms/ik-button/ik-button.stories.js
@@ -4,13 +4,119 @@ import {html} from "lit";
 export default {
     title: 'Atoms/ik-button',
     tags: ['autodocs'],
+    argTypes: {
+        type: {
+            control: { type: 'select' },
+            options: ['full', 'outlined', 'blue', 'red', 'icon'],
+        },
+    },
 };
 
 const Template = (args) => {
-    return html`<ik-button .text=${args.text}></ik-button>`;
+    return html`<ik-button 
+            .text=${args.text} 
+            .width=${args.width} 
+            .height=${args.height} 
+            .fontSize=${args.fontSize} 
+            .img=${args.img}
+            .icon=${args.icon}
+            .iconSize=${args.iconSize}
+            .type=${args.type}
+    ></ik-button>`;
 };
 
 export const Default = Template.bind({});
 Default.args = {
     text: 'Click !',
+    width: 'auto',
+    height: 'auto',
+    fontSize: '25px',
+    img: undefined,
+    icon: undefined,
+    iconSize: '',
+    type: 'full',
+};
+
+export const WithImg = Template.bind({});
+WithImg.args = {
+    text: 'Click !',
+    width: 'auto',
+    height: 'auto',
+    fontSize: '25px',
+    img: 'https://www.clever-cloud.com/app/themes/Starter/assets/img/brand-assets/square-png.png',
+    iconSize: '40px',
+    type: 'full',
+};
+
+export const Outlined = Template.bind({});
+Outlined.args = {
+    text: 'Click !',
+    width: 'auto',
+    height: 'auto',
+    fontSize: '25px',
+    type: 'outlined',
+};
+
+export const Blue = Template.bind({});
+Blue.args = {
+    text: 'Click !',
+    width: 'auto',
+    height: 'auto',
+    fontSize: '25px',
+    type: 'blue',
+};
+
+export const Red = Template.bind({});
+Red.args = {
+    text: 'Click !',
+    width: 'auto',
+    height: 'auto',
+    fontSize: '25px',
+    type: 'red',
+};
+
+const TemplateIcon = (args) => {
+    return html`
+        <div style="display: flex; flex-direction: row">
+            <ik-button 
+                .text=${args.text} 
+                .width=${args.width} 
+                .height=${args.height} 
+                .fontSize=${args.fontSize} 
+                .icon=${args.icon}
+                .iconSize=${args.iconSize}
+                .type=${args.type}
+            ></ik-button>
+            <ik-button 
+                .text=${args.text} 
+                .width=${args.width} 
+                .height=${args.height} 
+                .fontSize=${args.fontSize} 
+                .icon=${args.icon2}
+                .iconSize=${args.iconSize}
+                .type=${args.type}
+            ></ik-button>
+            <ik-button 
+                .text=${args.text} 
+                .width=${args.width} 
+                .height=${args.height} 
+                .fontSize=${args.fontSize} 
+                .icon=${args.icon3}
+                .iconSize=${args.iconSize}
+                .type=${args.type}
+            ></ik-button>
+        </div>
+    `;
+};
+
+export const Icon = TemplateIcon.bind({});
+Icon.args = {
+    width: 'auto',
+    height: 'auto',
+    fontSize: '25px',
+    icon: 'material-symbols:light-mode-outline-rounded',
+    icon2: 'material-symbols:dark-mode-outline-rounded',
+    icon3: 'material-symbols:add-circle-outline-rounded',
+    iconSize: '40px',
+    type: 'icon',
 };
