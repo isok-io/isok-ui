43c279fb225df1382c86ff5611458d08654eac18
Siori9 <clara.layus@hotmail.fr>
2025-05-13 11:33:11 +0200

    feat(components): add css theme management

--- packages/components/.storybook/preview.js
+++ packages/components/.storybook/preview.js
@@ -1,13 +1,29 @@
-/** @type { import('@storybook/web-components').Preview } */
-const preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
+import '../styles/light-theme.css';
+import '../styles/dark-theme.css';
+import '../styles/preview.css';
+
+export const globalTypes = {
+    theme: {
+        name: 'Theme',
+        description: 'Global theme for components',
+        defaultValue: 'light',
+        toolbar: {
+            icon: 'paintbrush',
+            items: [
+                { value: 'light', title: 'Light' },
+                { value: 'dark', title: 'Dark' },
+            ],
+        },
     },
-  },
 };
 
-export default preview;
+export const decorators = [
+    (storyFn, context) => {
+        const { theme } = context.globals;
+
+        document.documentElement.classList.remove('light', 'dark');
+        document.documentElement.classList.add(theme);
+
+        return storyFn();
+    },
+];
