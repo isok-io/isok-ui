83e2fba1875af0a05f0f71a727e1dcdd3c55b249
Siori9 <clara.layus@hotmail.fr>
2025-05-20 16:37:24 +0200

    feat(ui): add account page

---
+++ packages/ui/src/pages/Account.vue
@@ -0,0 +1,17 @@
+<script>
+import '@components-lit/molecules/other/ik-user-account/ik-user-account.js'
+
+</script>
+
+<template>
+  <ik-user-account
+      email="test@example.com"
+      @click-logout="() => console.log('logout')"
+      @click-manage-organizations="() => $router.push('/organizations')"
+      @click-delete-account="() => console.log('delete-account')"
+  ></ik-user-account>
+</template>
+
+<style scoped>
+
+</style>
