733bb56dd9890692bc4281271096d9ae2c3ce49b
Siori9 <clara.layus@hotmail.fr>
2025-05-20 15:26:33 +0200

    feat(ui): init app ui

---
+++ packages/ui/src/App.vue
@@ -0,0 +1,74 @@
+<script>
+import '@components-lit/molecules/other/ik-header/ik-header.js'
+
+export default {
+  data() {
+    return {
+      theme: 'light',
+    };
+  },
+  mounted() {
+    this.setTheme(this.theme);
+  },
+  methods: {
+    toggleTheme() {
+      this.theme = this.theme === 'light' ? 'dark' : 'light';
+      this.setTheme(this.theme);
+    },
+    setTheme(theme) {
+      document.documentElement.classList.remove('light', 'dark');
+      document.documentElement.classList.add(theme);
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="app">
+    <ik-header
+        class="header"
+        :optionsSelect="[
+          { value: 'orga1', label: 'MyOrganisation' },
+          { value: 'orga2', label: 'OtherOrganisation' },
+        ]"
+        fontSizeTitle="4em"
+        fontSizeSelect="1.2em"
+        widthSelect="20vw"
+        iconSize="1.7em"
+        connected=true
+        width="97vw"
+        :iconTheme="this.theme === 'dark' ? 'material-symbols:dark-mode-outline-rounded' : 'material-symbols:light-mode-outline-rounded'"
+        @change-theme="toggleTheme()"
+        @click-account="console.log('click account')"
+        @change-organization-value="(e) => console.log('change-organization-value', e)"
+    ></ik-header>
+    <router-view />
+  </div>
+</template>
+
+<style>
+.app {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  transition: background-color 0.3s ease;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.header {
+  z-index: 10;
+}
+
+:root.light .app {
+  background-color: #D9D9D9;
+}
+
+:root.dark .app {
+  background-color: #242424;
+}
+</style>
---
+++ packages/ui/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from "node:path";
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    target: 'esnext',
+  },
+  resolve: {
+    alias: {
+      '@components-lit': path.resolve(__dirname, '../components/components')
+    }
+  }
+})
